cba336e2fd6a0832125d4a8f1488d536b2fa6537
argenischaconb <argenischaconb@gmail.com>
2025-05-20 17:44:27 -0400

    fix: use toLowerCase for search by word

--- src/main/java/com/example/ejercicios_practicos_curso/service/CursoServiceImpl.java
+++ src/main/java/com/example/ejercicios_practicos_curso/service/CursoServiceImpl.java
@@ -39,7 +39,7 @@ public class CursoServiceImpl implements CursoService {
     @Override
     public List<Curso> getCoursesContainingWord(String word) {
         return cursoRepository.findAll().stream()
-                .filter((curso -> curso.getNombre().toLowerCase().contains(word)))
+                .filter((curso -> curso.getNombre().toLowerCase().contains(word.toLowerCase())))
                 .toList();
     }
 }
